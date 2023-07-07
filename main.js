a25a35f8b0d094e1019ed154dd7541e1f9238e8a
Arik Alexandrov <arikxl@gmail.com>
2023-07-08 00:47:11 +0300

    hammer position

--- main.js
+++ main.js
@@ -1,64 +1,116 @@
 const holes = [...document.querySelectorAll('.hole')];
 const scoreLabel = document.getElementById('scoreSpan')
+const LifeLabel = document.getElementById('lifeSpan')
+const hammer = document.getElementById('hammer1');
+const board = document.getElementById('board');
+const img = document.createElement('img');
 
+const live ="❤️"
+let lives = 3;
 let score = 0;
+let position = 4;
+let player1 = holes[position];
 
+LifeLabel.innerHTML=live.repeat(lives)
 
-function startGame() {
-    const player = holes[4];
-    const img = document.createElement('img');
+function addMole() {
     img.classList.add('mole');
-    img.src = "assets/mole.png";
-    player.appendChild(img);
+    img.src = "assets/img/mole.png";
+    player1.appendChild(img);
+}
+addMole();
+
+console.log('board.style.top:', board.style.top)
+
+
+
+function moveRight() {
+    if(position===2 || position===5 || position===8) return;
+    player1 = holes[position+=1];
+    addMole();
+}
+function moveLeft() {
+    if(position===0 || position===3 || position===6) return;
+    player1 = holes[position-=1];
+    addMole();
+}
+function moveDown() {
+    if(position>5) return;
+    player1 = holes[position+=3];
+    addMole();
+
+}
+function moveUp() {
+    if(position<3) return;
+    player1 = holes[position -= 3];
+    addMole();
 }
 
-startGame()
-
-function moveMole() {
-    const player = holes[1];
-    player.classList.add('player');
-
-
-
-}
-moveMole();
-
-    document.addEventListener('keydown', function(event) {
-  console.log('event:', event)
-  // Check if the pressed key is the Enter key (key code 13)
-  if (event.keyCode === 13) {
-    // Perform some action
-    console.log('Enter key pressed!');
+document.addEventListener('keydown', function (event) {
+  if (event.code === 'ArrowRight') {
+      moveRight()   
+  }
+  if (event.code === 'ArrowLeft') {
+      moveLeft()   
+  }
+  if (event.code === 'ArrowUp') {
+    moveUp()
+  }
+  if (event.code === 'ArrowDown') {
+    moveDown()
   }
 });
 
 
+function moveHammer() {
+    hammer.classList.add('active');
+    const boardWidth = board.offsetWidth;
+    const boardHeight = board.offsetHeight;   
 
+    // Generate random positions within the board's dimensions
+    const posX = ~~(Math.random() * (boardWidth - hammer.offsetWidth));
+    const posY = ~~(Math.random() * (boardHeight - hammer.offsetHeight));
 
-
-function run() {
-    const i = Math.floor(Math.random() * holes.length); 
-    const hole = holes[i];
-    let timer = null;
-
-    const img = document.createElement('img');
-    img.classList.add('mole');
-    img.src = "assets/mole.png";
-
+  // Update the position of the hammer
+    hammer.style.left = posX + 'px';
+    hammer.style.top = 500 + 'px';
+    // hammer.style.top = posY + 'px';
     
-    img.addEventListener('click', () => {
-        img.src = "assets/mole-whacked.png";
-        score +=10;
-        scoreLabel.innerHTML = score;
-    })
+    setTimeout(() => {
+        hammer.classList.remove('active');
+    }, 600);
+    console.log(hammer.style.top)
     
-    // hole.appendChild(img);
-
-    timer = setTimeout(() => {
-        hole.appendChild(img);
-        run()
-    }, 1500)
 }
+
+setInterval(moveHammer, 1200);
+
+
+
+
+// function run() {
+//     const i = Math.floor(Math.random() * holes.length); 
+//     const hole = holes[i];
+//     let timer = null;
+
+//     const img = document.createElement('img');
+//     img.classList.add('mole');
+//     img.src = "assets/mole.png";
+
+    
+//     img.addEventListener('click', () => {
+//         img.src = "assets/mole-whacked.png";
+//         score +=10;
+//         scoreLabel.innerHTML = score;
+//     })
+    
+//     // hole.appendChild(img);
+
+//     timer = setTimeout(() => {
+//         hole.appendChild(img);
+//         run()
+//     }, 1500)
+// }
 
 // run()
 
